7938d630dd20692809dccc9af3fd1b5567d66faa
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-08-28 05:42:37 +0900

    c hello world

---
+++ hellloWorld.c
@@ -0,0 +1,17 @@
+/*
+ * hellloWorld.c
+ *
+ *  Created on: 2018/08/28
+ *      Author: hp
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char c [20];
+	strcpy(c,"Hello,World");
+	printf("%s\n",c);
+}
